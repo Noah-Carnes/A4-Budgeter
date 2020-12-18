a07ac8f210692537bf2e9cc7974dba3ec764b036
kelzyth <2004810@apps.nsd.org>
2020-12-18 08:39:35 -0800

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,49 @@
+/*
+Noah Carnes
+Reiber
+AP Programming in Java
+
+This program prompts a person for income and expense amounts, then calculates their net monthly
+income.
+
+Expected Output:
+
+This program asks for your monthly income and
+expenses, then tells you your net monthly income.
+How many categories of income? 3
+Next income amount? $1000
+Next income amount? $250.25
+Next income amount? $175.50
+Enter 1) monthly or 2) daily expenses? 1
+How many categories of expense? 4
+Next expense amount? $850
+Next expense amount? $49.95
+Next expense amount? $75
+Next expense amount? $120.67
+Total income = $1425.75 ($45.99/day)
+Total expenses = $1095.62 ($35.34/day)
+You earned $330.13 more than you spent this month.
+You're a big saver.
+<< Your custom message goes here >>
+
+ */
+
+
+package Main;
+import java.util.*;
+
+
+class Main {
+public static void main(String[] args){
+     Scanner scanner = new Scanner(System.in);
+
+
+}
+public static int askCategories(Scanner scanner){
+     
+     return 0;
+
+}
+
+}
+
